8e566f86d748892eca38d9b9126e2d659c960117
Ramya <ramyayadav2016@gmail.com>
2023-12-19 17:25:19 +0530

    first react component

--- React w Redux/taskopedia/src/index.js
+++ React w Redux/taskopedia/src/index.js
@@ -2,9 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function MainBody()
+{
+  return(
+  <div>
+      <p>Ramya, you are the best!</p>
+      <ul>
+        <li>Ramya</li>
+        <li>is the prettiesttttt</li>
+      </ul>
+    </div>
+  );
+}
+
+//you can render like below
+//root.render(MainBody());
+
+//or render like this as well
+//root.render(<MainBody />);
+
+//or something like below
 root.render(
   <div>
-    Hi!
+    <MainBody />
+    <MainBody />
+    <MainBody />
   </div>
-);
-
+)
